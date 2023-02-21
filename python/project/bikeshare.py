075feeab65172b166b86276b1dff4ee51f056bd4
zejuniortdr <zejuniortdr@gmail.com>
2023-02-21 12:20:36 -0300

    precommit

--- python/project/bikeshare.py
+++ python/project/bikeshare.py
@@ -1,24 +1,49 @@
+from __future__ import absolute_import, print_function
+
 import time
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+from six.moves import input
 
-
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
-MONTHS_NAMES = ['january', 'february', 'march', 'april', 'may', 'june']
-WEEKDAYS_NAMES = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+CITY_DATA = {
+    "chicago": "chicago.csv",
+    "new york city": "new_york_city.csv",
+    "washington": "washington.csv",
+}
+MONTHS_NAMES = ["january", "february", "march", "april", "may", "june"]
+WEEKDAYS_NAMES = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+]
 
 
 def get_input_data(label, validation_data, enable_all=False):
-     while True:
-        user_input = input(f"Enter the {label} name: ").lower()
-        if user_input in validation_data or (user_input=='all' and enable_all):
-            return user_input
-        else:
-            print(f'Invalid {label} name [{user_input}]. Please choose one of the following: {validation_data}')
+    """
+    Generic function to get data from input and validate until is correct
 
-         
+    Returns:
+        (str) label - label for messages to user like city, month and day
+        (list) validation_data - validation list to be compare against user input
+        (boolean) enable_all - allow the user to type [all] for a filter
+    """
+    while True:
+        enable_all_error_message = ""
+        user_input = eval(input(f"Enter the {label} name: ").lower())
+        if user_input in validation_data or (user_input == "all" and enable_all):
+            return user_input
+        elif enable_all:
+            enable_all_error_message = " or type [all]"
+
+        print(
+            f"Invalid {label} name [{user_input}]. Please choose one of the following: {validation_data}{enable_all_error_message}"
+        )
+
 
 def get_filters():
     """
@@ -29,7 +54,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print("Hello! Let's explore some US bikeshare data!")
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = get_input_data("city", list(CITY_DATA.keys()))
 
@@ -39,7 +64,7 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
     day = get_input_data("day of week", WEEKDAYS_NAMES, enable_all=True)
 
-    print('-'*40)
+    print(("-" * 40))
     return city, month, day
 
 
@@ -55,81 +80,69 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
 
-
     return df
 
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
-    print('\nCalculating The Most Frequent Times of Travel...\n')
+    print("\nCalculating The Most Frequent Times of Travel...\n")
     start_time = time.time()
 
     # display the most common month
 
-
     # display the most common day of week
-
 
     # display the most common start hour
 
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(("\nThis took %s seconds." % (time.time() - start_time)))
+    print(("-" * 40))
 
 
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
-    print('\nCalculating The Most Popular Stations and Trip...\n')
+    print("\nCalculating The Most Popular Stations and Trip...\n")
     start_time = time.time()
 
     # display most commonly used start station
 
-
     # display most commonly used end station
-
 
     # display most frequent combination of start station and end station trip
 
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(("\nThis took %s seconds." % (time.time() - start_time)))
+    print(("-" * 40))
 
 
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
-    print('\nCalculating Trip Duration...\n')
+    print("\nCalculating Trip Duration...\n")
     start_time = time.time()
 
     # display total travel time
 
-
     # display mean travel time
 
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(("\nThis took %s seconds." % (time.time() - start_time)))
+    print(("-" * 40))
 
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
-    print('\nCalculating User Stats...\n')
+    print("\nCalculating User Stats...\n")
     start_time = time.time()
 
     # Display counts of user types
 
-
     # Display counts of gender
-
 
     # Display earliest, most recent, and most common year of birth
 
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(("\nThis took %s seconds." % (time.time() - start_time)))
+    print(("-" * 40))
 
 
 def main():
@@ -142,10 +155,10 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        restart = eval(input("\nWould you like to restart? Enter yes or no.\n"))
+        if restart.lower() != "yes":
             break
 
 
 if __name__ == "__main__":
-	main()
+    main()
